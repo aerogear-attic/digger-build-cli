d6596a78518869d030028f6b6f5b6e3ff7abd49c
Ali Ok <aliok@apache.org>
2016-10-10 15:19:26 +0300

    Refactor if-else to a dict-lambda for picking correct builder for a project

--- digger/builds/__init__.py
+++ digger/builds/__init__.py
@@ -1,9 +1,31 @@
 import os
 
+from digger import errors
+
 from .ant import AntBuild
 from .cordova_android import CordovaAndroidBuild
 from .gradle import GradleBuild
 
+ref = {
+  'gradle': {
+    'is_valid': lambda path: os.path.exists('%s/build.gradle' % path),
+    'builder': lambda path: GradleBuild.from_path(path)
+  },
+  'ant': {
+    'is_valid': lambda path: os.path.exists('%s/build.xml' % path),
+    'builder': lambda path: AntBuild.from_path(path)
+  },
+  'cordova_android': {
+    # one big improvement would be:
+    # * parse config.xml file
+    # * check if the root element is "widget" from namespace "http://www.w3.org/ns/widgets"
+    # then we can be sure that it is a Cordova project.
+    # we also need to check if we want to build an IOS app or Android app for the Cordova.
+    # for now, we don't do that and support Android only.
+    'is_valid': lambda path: os.path.exists('%s/config.xml' % path),
+    'builder': lambda path: CordovaAndroidBuild.from_path(path)
+  }
+}
 
 def from_path(path):
   """
@@ -11,17 +33,9 @@ def from_path(path):
 
   :param path(str): required path argument to be used
   """
-  if os.path.exists('%s/build.gradle' % path):
-    return GradleBuild.from_path(path)
-
-  elif os.path.exists('%s/config.xml' % path):
-    # one big improvement would be:
-    # * parse config.xml file
-    # * check if the root element is "widget" from namespace "http://www.w3.org/ns/widgets"
-    # then we can be sure that it is a Cordova project.
-    # we also need to check if we want to build an IOS app or Android app for the Cordova.
-    # for now, we don't do that and support Android only.
-    return CordovaAndroidBuild.from_path(path)
-
-  else:
-    return AntBuild.from_path(path)
+  for item in ref:
+    build = ref[item]
+    valid_ = build['is_valid']
+    if valid_(path) is True:
+      return build['builder'](path)
+  raise errors.InvalidProjectStructure()
